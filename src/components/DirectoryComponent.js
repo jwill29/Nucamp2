c411effa190dd5129658074b6d8e87f77b9dda3b
Joyce Willett <joycewillett@hotmail.com>
2021-04-10 12:50:39 -0700

    Week 2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
 
 class Directory extends Component {
     constructor(props) {
@@ -10,22 +11,7 @@ class Directory extends Component {
     }
 
     onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
+        this.setState({ selectedCampsite: campsite });
     }
 
     render() {
@@ -33,10 +19,10 @@ class Directory extends Component {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
                     <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                    <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                    <CardImgOverlay>
-                        <CardTitle>{campsite.name}</CardTitle>
-                    </CardImgOverlay>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
                     </Card>
                 </div>
             );
@@ -47,11 +33,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+                <CampsiteInfo campsite={this.state.selectedCampsite} />
             </div>
         );
     }
