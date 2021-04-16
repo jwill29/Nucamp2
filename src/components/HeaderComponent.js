a6b02a41346f4f11aa2ef97292c5edc0d50e392e
Joyce Willett <joycewillett@hotmail.com>
2021-04-15 21:42:52 -0700

    React Router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,24 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false
+        };
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
